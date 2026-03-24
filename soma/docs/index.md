1872db07580c45f2de30cfe2efefcaf104c1c90f
marianamellerjost-cpu <marianamellerjost@gmail.com>
2026-03-24 20:48:12 -0300

    Add files via upload

---
+++ soma/docs/index.md
@@ -0,0 +1,3 @@
+# Documentação 
+
+Este programa lê dois números do teclado, calcula a soma e coloca o resultado na tela.
---
+++ soma/soma/soma_0-0-1.py
@@ -0,0 +1,29 @@
+"""
+Programa de soma
+Descrição: Este programa lê dois números do teclado, calcula a soma e coloca o resultado na tela.
+Autor: Mariana Jost
+Data: 24/03/2026
+Versão: 0.0.1
+
+"""
+
+# Alocação de dados
+
+num1 = 0
+
+num2 = 0
+
+soma = 0 
+
+# Entrada de dados
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Processamento de dados
+
+soma = num1 + num2 
+
+# Saída de dados
+
+print("A soma dos números é", soma)
