4099444a001fc670c51a6cf917cdc3a1c67c535b
marianamellerjost-cpu <marianamellerjost@gmail.com>
2026-03-24 19:12:14 -0300

    Add files via upload

---
+++ imprime5/docs/imprime5.md
@@ -0,0 +1,15 @@
+# Documentação 
+
+Programa imprime5
+Descrição: Este programa imprime os números inteiros de 1 a 5.
+Autor: Mariana Meller Jost
+Data: 24/03/2026
+Versão: 0.0.1
+
+## Introdução 
+
+
+## Funcionalidade
+
+
+## Conclusão 
---
+++ imprime5/tests/imprime5_001.py
@@ -0,0 +1,24 @@
+"""
+Programa imprime5
+Descrição: Este programa imprime os números inteiros de 1 a 5.
+Autor: Mariana Meller Jost
+Data: 24/03/2026
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+
+i = 0
+
+# Entrada de dados
+
+
+# Processamento de dados
+
+
+# Saída de dados
+
+
+while i < 5:
+    print(i+1)
+    i = i + 1 
